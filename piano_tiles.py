bdf5c99bb1facbd1e5597cd57d4b0b0fd054347a
Lucas Brasil Silva <lucasbrasil396@gmail.com>
2023-10-10 16:08:42 -0300

    Update Code

--- piano_tiles.py
+++ piano_tiles.py
@@ -12,32 +12,35 @@ def click(x,y):
     sleep(0.05)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-# Abre no navegador o site do jogo:
-webbrowser.open(
-    'https://sandbox.gameforge.com/en-US/littlegames/magic-piano-tiles/')
+def main():
+    # Abre no navegador o site do jogo:
+    webbrowser.open('https://sandbox.gameforge.com/en-US/littlegames/magic-piano-tiles/')
 
-# Espera o site carregar e clicar em play:
-sleep(10)
-pyautogui.click(2090, 501,duration=1)
+    # Espera o site carregar e clicar em play:
+    sleep(15)
+    pyautogui.click(2090, 501,duration=1)
 
-# Aguarda os anúncios finalizarem, então clicar em start:
-sleep(70)
-pyautogui.click(2142,467,duration=1)
+    # Aguarda os anúncios finalizarem, então clicar em start:
+    sleep(70)
+    pyautogui.click(2142,467,duration=1)
 
-# Verificar se a barra preta chegou no lugar e executa o click,
-# Fara isso quanto não for pressionado a tecla 'space' ou até errar o click:
-while keyboard.is_pressed('space') == False:
-    if pyautogui.pixelMatchesColor(2008,390,(0, 0, 0)):
-        click(2008,390)
+    # Verificar se a barra preta chegou no lugar e executa o click,
+    # Fara isso quanto não for pressionado a tecla 'space' ou até errar o click:
+    while keyboard.is_pressed('space') == False:
+        if pyautogui.pixelMatchesColor(2008,390,(0, 0, 0)):
+            click(2008,390)
 
-    if pyautogui.pixelMatchesColor(2095,387,(0, 0, 0)):
-        click(2095,387)
+        if pyautogui.pixelMatchesColor(2095,387,(0, 0, 0)):
+            click(2095,387)
 
-    if pyautogui.pixelMatchesColor(2174,378,(0, 0, 0)):
-        click(2174,378)
+        if pyautogui.pixelMatchesColor(2174,378,(0, 0, 0)):
+            click(2174,378)
 
-    if pyautogui.pixelMatchesColor(2257,379,(0, 0, 0)):
-        click(2257,379)
-    
-    if pyautogui.pixelMatchesColor(2056, 460, (255, 255, 255)):
-        break
+        if pyautogui.pixelMatchesColor(2257,379,(0, 0, 0)):
+            click(2257,379)
+        
+        if pyautogui.pixelMatchesColor(2056, 460, (255, 255, 255)):
+            break
+
+if __name__ == '__main__':
+    main()
